8d129c1a827731cae181976d16d13ffe6dc0d4ad
makeworld <colecmac@protonmail.com>
2020-06-02 15:50:09 -0400

    📄 Update license classifier for next release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
         "Topic :: Text Processing",
         "Topic :: Text Processing :: General",
