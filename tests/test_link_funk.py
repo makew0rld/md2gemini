7390730e270198272aaed0a8fa4ce0ee44d62816
Guy Godfroy <guy.godfroy@gugod.fr>
2020-12-05 08:16:52 +0100

    ✅ add test for link_func

---
+++ tests/test_link_funk.py
@@ -0,0 +1,20 @@
+from .util import normalize
+from md2gemini import md2gemini
+
+
+def f(md):
+    return normalize(md2gemini(md, link_func=func))
+
+def func(link):
+    return foo
+
+def test_func_root_url():
+    md = "[test](/url)"
+    gem = "=> foo test"
+    assert f(md) == gem
+
+
+def test_func_absolute_url():
+    md = "[test](https://duck.com/test)"
+    gem = "=> foo test"
+    assert f(md) == gem
