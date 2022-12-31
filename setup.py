80f9fc5354faf995b680c10d1389797ba126fd97
makeworld <makeworld@protonmail.com>
2022-12-30 19:39:31 -0500

    See #42

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/makeworld-the-better-one/md2gemini",
-    packages=setuptools.find_packages(),
+    packages=["md2gemini"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
